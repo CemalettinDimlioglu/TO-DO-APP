1eccc6187dfbbeaa0e35d9ddcf62565a384ef721
CMLTTN <cemaldim@gmail.com>
2023-05-03 11:35:40 +0300

    js

---
+++ app.js
@@ -0,0 +1,4 @@
+const container  = document.getElementById("container");
+const item = document.querySelector(".item")
+const input = document.querySelector("#input")
+const button = document.querySelector("#button")
