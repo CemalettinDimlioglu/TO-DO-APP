8a290b2f35ea347bc8945553752e01f1e2e9a54f
CMLTTN <cemaldim@gmail.com>
2023-05-03 11:55:33 +0300

    js if

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ const container  = document.getElementById("container");
 const item = document.querySelector(".item")
 const input = document.querySelector("#input")
 const button = document.querySelector("#button")
+
+button.addEventListener("click", (e)=>{
+   if (input.value.trim()===""){
+     alert("please enter new to do text!");
+     return;
+   }  
+});
