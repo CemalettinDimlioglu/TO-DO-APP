1ffb9f5026eca0cc2f17659378f168dd5903c15e
CMLTTN <cemaldim@gmail.com>
2023-05-04 12:09:20 +0300

    aa

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const todoInput = document.getElementById("todo-input");
 const addBtn = document.querySelector("#todo-button");
 const todoUl = document.querySelector("#todo-ul");
-let todoList = [];
+let todoList = JSON.parse(localStorage.getItem("todoList")) || []
+
+
+window.addEventListener("load",()=> {
+  
+})
 addBtn.addEventListener("click", (e) => {
   e.preventDefault();
   if (todoInput.value.trim() === "") {
@@ -34,6 +39,23 @@ const createTodo = (newTodo) => {
   todoUl.prepend(li);
 };
 
-todoUl.addEventListener("click",(e)=>{
-  const idAttr =e.target.classList.contains("li").getAttribute
-})
+todoUl.addEventListener("click", (e) => {
+  const idAttr = e.target.classList.contains("li").getAttribute("id");
+  if (e.target.classList.contains("fa-check")) {
+    e.target.parentElement.classList.toggle("checked");
+    todoList.forEach((todo) => {
+      if (todo.id == idAttr) {
+        todo.completed =!todo.completed;
+      }
+    });
+  } else if (e.target.classList.contains("fa-trash")) {
+    e.target.parentElement.remove();
+    todoList=todoList.filter((todo)=>todo.id != idAttr)
+
+
+
+
+  } else {
+    alert("other element click");
+  }
+});
