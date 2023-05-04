fbff2a919aa0872be140670114a5a0751b647ae9
CMLTTN <cemaldim@gmail.com>
2023-05-04 10:57:04 +0300

    push

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
- const todoInput = document.getElementsById("todo-input");
- const addBtn = document.querySelector("#todo-button");
- const todoUl = document.querySelector("#todo-ul");
+const todoInput = document.getElementsById("todo-input");
+const addBtn = document.querySelector("#todo-button");
+const todoUl = document.querySelector("#todo-ul");
+let todoList =[];
 
- addBtn.addEventListener("click",(e)=>{
-
+addBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  if(todoInput.value.trim()===""){
-    alert("Please enter new todo text!")
+  if (todoInput.value.trim() === "") {
+    alert("Please enter new todo text!");
+    return;
+  } else {
+    const newTodo = {
+      id: new Date().getTime(),
+      completed: false,
+      text: todoInput.value,
+    };
+    todoList.push(newTodo);
+
   }
- })
+});
