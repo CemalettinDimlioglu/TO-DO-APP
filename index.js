31171245e977e9ebe38c74183465c0ab07a4d54f
CMLTTN <cemaldim@gmail.com>
2023-05-04 22:19:19 +0300

    done

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-const todoInput = document.getElementById("todo-input");
+ const todoInput = document.getElementById("todo-input");
 const addBtn = document.querySelector("#todo-button");
 const todoUl = document.querySelector("#todo-ul");
-let todoList = JSON.parse(localStorage.getItem("todoList")) || []
-
-
-window.addEventListener("load",()=> {
-  
-})
+let todoList = JSON.parse(localStorage.getItem("todoList")) || [];
+window.addEventListener("load", () => {
+  getTodoListFromLocalStorage();
+});
+const getTodoListFromLocalStorage = () => {
+  todoList.forEach((todo) => {
+    createTodo(todo);
+  });
+};
 addBtn.addEventListener("click", (e) => {
   e.preventDefault();
   if (todoInput.value.trim() === "") {
     alert("Please enter new todo text!");
     return;
-  } else {
-    const newTodo = {
-      id: new Date().getTime(),
-      completed: false,
-      text: todoInput.value,
-    };
-    todoList.push(newTodo);
-    createTodo(newTodo);
   }
+  const newTodo = {
+    id: new Date().getTime(),
+    completed: false,
+    text: todoInput.value,
+  };
+  createTodo(newTodo);
+  todoList.push(newTodo);
+  localStorage.setItem("todoList", JSON.stringify(todoList));
+  e.target.closest("form").reset();
 });
 const createTodo = (newTodo) => {
   const { id, completed, text } = newTodo;
@@ -38,24 +42,22 @@ const createTodo = (newTodo) => {
   li.append(removeIcon);
   todoUl.prepend(li);
 };
-
 todoUl.addEventListener("click", (e) => {
-  const idAttr = e.target.classList.contains("li").getAttribute("id");
+  const idAttr = e.target.closest("li").getAttribute("id");
   if (e.target.classList.contains("fa-check")) {
     e.target.parentElement.classList.toggle("checked");
     todoList.forEach((todo) => {
       if (todo.id == idAttr) {
-        todo.completed =!todo.completed;
+        todo.completed = !todo.completed;
       }
     });
+    localStorage.setItem("todoList", JSON.stringify(todoList));
   } else if (e.target.classList.contains("fa-trash")) {
     e.target.parentElement.remove();
-    todoList=todoList.filter((todo)=>todo.id != idAttr)
-
-
-
-
+    todoList = todoList.filter((todo) => todo.id != idAttr);
+    localStorage.setItem("todoList", JSON.stringify(todoList));
   } else {
-    alert("other element click");
+    alert("other element clicked");
   }
+  console.log(todoList);
 });
