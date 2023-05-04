c758a9a0f8f25e9000b7436dd242858f56b9c38c
CMLTTN <cemaldim@gmail.com>
2023-05-04 10:43:03 +0300

    if alert

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
  const todoInput = document.getElementsById("todo-input");
  const addBtn = document.querySelector("#todo-button");
  const todoUl = document.querySelector("#todo-ul");
- 
+
+ addBtn.addEventListener("click",(e)=>{
+
+  e.preventDefault();
+  if(todoInput.value.trim()===""){
+    alert("Please enter new todo text!")
+  }
+ })
